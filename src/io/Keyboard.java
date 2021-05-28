bd738f2a60fbbb9c71da4564a9ce7ef4c6f439fc
Hunter T <strangeranger01@randomserver.xyz>
2021-05-27 18:26:53 -0700

    Modify code and remove unused methods

--- src/io/Keyboard.java
+++ src/io/Keyboard.java
@@ -4,77 +4,37 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-// TODO: Implement singleton pattern on the keyboard.
 /**
  * Facilitates keyboard input by abstracting details about input parsing, conversions,
  * and exception handling.
+ *
+ * TODO: Implement singleton pattern on the keyboard.
  */
 public class Keyboard {
-    //****[ Variable Declaration ]****************************************************\\
-
-    /// Error Handling Variables
-    private static boolean printErrors = true;
-    private static int     errorCount  = 0;
-
-    /// Tokenized Input Stream Variables
-    private static String          currentToken = null;
-    private static StringTokenizer reader;
-    private static BufferedReader  in
+    // Tokenized Input Stream Variables
+    private static String               currentToken = null;
+    private static StringTokenizer      reader;
+    private static final BufferedReader in
             = new BufferedReader(new InputStreamReader(System.in));
-    
-    //****[ Error Handling Methods ]**************************************************\\
-
-    /** Returns the current error count. */
-    public static int getErrorCount() {
-        return errorCount;
-    }
-
-    /** Resets the current error count to zero. */
-    public static void resetErrorCount() {
-        errorCount = 0;
-    }
 
     /**
-     * Returns a boolean indicating whether input errors are currently printed to
-     * standard output.
-     */
-    public static boolean getPrintErrors() {
-        return printErrors;
-    }
-
-    /**
-     * Sets a boolean indicating whether input errors are to be printed to standard
-     * output.
-     *
-     * @param flag True or false...
-     */
-    public static void setPrintErrors(boolean flag) {
-        printErrors = flag;
-    }
-
-    /**
-     * Increments the error count and prints the error message if appropriate.
+     * Prints the error messagE.
      *
      * @param str Error text to be printed.
      */
     private static void error(String str) {
-        errorCount++;
-
-        if (printErrors) {
-            System.out.println(str);
-        }
+        System.out.println(str);
     }
-
-    //****[ Tokenized Input Stream Section ]******************************************\\
 
     /** Gets the next input token assuming it may be on subsequent input lines. */
     private static String getNextToken() {
         return getNextToken(true);
     }
 
-    // TODO: Add more to method comments.
     /**
      * Gets the next input token, which may already have been read.
+     *
+     * TODO: Add more to method comments.
      *
      * @param skip ...
      * @return 	   ...
@@ -88,14 +48,14 @@ public class Keyboard {
             token        = currentToken;
             currentToken = null;
         }
-
         return token;
     }
 
-    // TODO: Add more to method comments.
     /**
      * Gets the next token from the input, which may come from the current input line or
      * a subsequent one. The parameter determines if subsequent lines are used.
+     *
+     * TODO: Add more to method comments.
      *
      * @param skip ...
      * @return     ...
@@ -131,51 +91,19 @@ public class Keyboard {
 
     /** Returns a string read from standard input. */
     public static String readString() {
-        String str;
+        StringBuilder str;
 
         try {
-            str = getNextToken(false);
+            str = new StringBuilder(getNextToken(false));
             while (! endOfLine()) {
-                str = str + getNextToken(false);
+                str.append(getNextToken(false));
             }
         } catch (Exception exception) {
             error("Error reading String data, null value returned.");
             str = null;
         }
-        return str;
-    }
-
-    /** Returns a space-delimited substring (a word) read from standard input. */
-    public static String readWord() {
-        String token;
-        try {
-            token = getNextToken();
-        } catch (Exception exception) {
-            error("Error reading String data, null value returned.");
-            token = null;
-        }
-        return token;
-    }
-
-    /** Returns a boolean read from standard input. */
-    public static boolean readBoolean() {
-        String token = getNextToken();
-        boolean bool;
-
-        try {
-            if (token.equalsIgnoreCase("true")) {
-                bool = true;
-            } else if (token.equalsIgnoreCase("false")) {
-                bool = false;
-            } else {
-                error("Error reading boolean data, false value returned.");
-                bool = false;
-            }
-        } catch (Exception exception) {
-            error("Error reading boolean data, false value returned.");
-            bool = false;
-        }
-        return bool;
+        assert str != null;
+        return str.toString();
     }
 
     /** Returns a character read from standard input. */
@@ -185,11 +113,11 @@ public class Keyboard {
 
         try {
             if (token.length() > 1) {
-                currentToken = token.substring(1, token.length());
+                currentToken = token.substring(1);
             } else {
                 currentToken = null;
             }
-            
+
             value = token.charAt(0);
         } catch (Exception exception) {
             error("Error reading char data, MIN_VALUE value returned.");
@@ -209,48 +137,6 @@ public class Keyboard {
         } catch (Exception exception) {
             error("Error reading int data, MIN_VALUE value returned.");
             value = Integer.MIN_VALUE;
-        }
-        return value;
-    }
-
-    /** Returns a long integer read from standard input. */
-    public static long readLong() {
-        String token = getNextToken();
-        long   value;
-
-        try {
-            value = Long.parseLong(token);
-        } catch (Exception exception) {
-            error("Error reading long data, MIN_VALUE value returned.");
-            value = Long.MIN_VALUE;
-        }
-        return value;
-    }
-
-    /** Returns a float read from standard input. */
-    public static float readFloat() {
-        String token = getNextToken();
-        float  value;
-
-        try {
-            value = Float.parseFloat(token);
-        } catch (Exception exception) {
-            error("Error reading float data, NaN value returned.");
-            value = Float.NaN;
-        }
-        return value;
-    }
-
-    /** Returns a double read from standard input. */
-    public static double readDouble() {
-        String token = getNextToken();
-        double value;
-
-        try {
-            value = Double.parseDouble(token);
-        } catch (Exception exception) {
-            error("Error reading double data, NaN value returned.");
-            value = Double.NaN;
         }
         return value;
     }
